b01534448dd9d4633d2974c2fadc17bc3c53b467
hastiafra <77177165+hastiafra@users.noreply.github.com>
2023-03-09 16:19:06 -0500

    style and getting input value

--- sources/src/App.js
+++ sources/src/App.js
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import "./App.css";
-
 import { Recipes } from "./helpers/fetch";
 
 function App() {
+  const { REACT_APP_SPOONACULAR, REACT_APP_SPOONACULAR_URL } = process.env;
+  const [searchFieldValue, setSearchFieldValue] = useState(null);
+  const handleSearch = async () => {
+    if (searchFieldValue) {
+      const searchUrl = `${REACT_APP_SPOONACULAR_URL}recipes/complexSearch?apiKey=${REACT_APP_SPOONACULAR}&query=${searchFieldValue}`;
+    }
+  };
+
   return (
     <div className="App">
       <div className="appWrapper">
@@ -11,9 +19,12 @@ function App() {
           <input
             className="search"
             type="search"
+            onChange={(e) => setSearchFieldValue(e.target.value)}
             placeholder="e.g., pasta, cake and soup"
           ></input>
-          <button className="searchButton">go!</button>
+          <button className="searchButton" onClick={handleSearch}>
+            go!
+          </button>
         </div>
       </div>
       <div className="overlay"></div>
