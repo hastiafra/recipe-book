be43a462e62a5f6a1d77a4ef79e1ef7d87385119
hastiafra <77177165+hastiafra@users.noreply.github.com>
2023-03-10 09:37:27 -0500

    create home pag

--- sources/src/App.js
+++ sources/src/App.js
@@ -1,32 +1,14 @@
 import { useState } from "react";
 import "./App.css";
-import { Recipes } from "./helpers/fetch";
+import { Recipes } from "./components/fetch";
+import Home from "./components/Home";
 
 function App() {
-  const { REACT_APP_SPOONACULAR, REACT_APP_SPOONACULAR_URL } = process.env;
-  const [searchFieldValue, setSearchFieldValue] = useState(null);
-  const handleSearch = async () => {
-    if (searchFieldValue) {
-      const searchUrl = `${REACT_APP_SPOONACULAR_URL}recipes/complexSearch?apiKey=${REACT_APP_SPOONACULAR}&query=${searchFieldValue}`;
-    }
-  };
+  const [recipeData, setRecipesData] = useState([]);
 
   return (
-    <div className="App">
-      <div className="appWrapper">
-        <p className="title">Please search for your recipes</p>
-        <div className="searchWrapper">
-          <input
-            className="search"
-            type="search"
-            onChange={(e) => setSearchFieldValue(e.target.value)}
-            placeholder="e.g., pasta, cake and soup"
-          ></input>
-          <button className="searchButton" onClick={handleSearch}>
-            go!
-          </button>
-        </div>
-      </div>
+    <div>
+      <Home setRecipesData={setRecipesData} recipeData={recipeData}/>
       <div className="overlay"></div>
     </div>
   );
---
+++ sources/src/components/Home.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./home.css";
+
+const Home = ({ setRecipesData, recipeData }) => {
+  const { REACT_APP_SPOONACULAR, REACT_APP_SPOONACULAR_URL } = process.env;
+
+  const [searchFieldValue, setSearchFieldValue] = useState(null);
+
+  const [error, setError] = useState(false);
+
+  const handleSearch = async () => {
+    if (searchFieldValue) {
+      const searchUrl = `${REACT_APP_SPOONACULAR_URL}recipes/complexSearch?apiKey=${REACT_APP_SPOONACULAR}&query=${searchFieldValue.toLowerCase()}`;
+      try {
+        const response = await fetch(searchUrl);
+        const data = await response.json();
+        setRecipesData(data.results);
+      } catch (er) {
+        console.log(er);
+      }
+    }
+    if (!recipeData.length || recipeData === undefined) {
+      setError(true);
+    }
+  };
+  return (
+    <>
+      <div className="homeWrapper">
+        <p className="homeTitle">Please search for your recipes</p>
+        <div className="searchWrapper">
+          <input
+            className="search"
+            type="search"
+            onChange={(e) => setSearchFieldValue(e.target.value)}
+            placeholder="e.g., pasta, cake and soup"
+          ></input>
+          <button className="searchButton" onClick={handleSearch}>
+            go!
+          </button>
+        </div>
+      </div>
+      <div className="overlay"></div>
+    </>
+  );
+};
+
+export default Home;
