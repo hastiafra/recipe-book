fa0b60b85d798cfc3e017b4cd0264ed578738fa0
hastiafra <77177165+hastiafra@users.noreply.github.com>
2023-03-10 09:53:18 -0500

    updating navigation

--- sources/src/App.js
+++ sources/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 
@@ -7,8 +8,16 @@ function App() {
 
   return (
     <div>
-      <Home setRecipesData={setRecipesData} recipeData={recipeData}/>
-      <div className="overlay"></div>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home setRecipesData={setRecipesData} recipeData={recipeData} />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
